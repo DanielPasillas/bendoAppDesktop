65e1ed32b331809e974e704d0f5811a33cd38464
bryan daniel pasillas rodriguez <brayan.pasillas.4@hotmail.com>
2019-07-30 13:05:55 -0500

    DbEntities added, and a global db connector.

--- bendoAppDesktop/App.xaml.cs
+++ bendoAppDesktop/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using bendoAppDesktop.DbContextEntities;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,9 @@ namespace bendoAppDesktop
     /// </summary>
     public partial class App : Application
     {
+        // DbContext Entities
+        private static bendo_puntoventaEntities _dbContext;
+
+        public static bendo_puntoventaEntities BendoDbContext { get => App._dbContext; set => App._dbContext = value; }
     }
 }
